f086893faabc3fafe7563757eb5d4c41d4606566
Wiggert <wloonstra@ebay.com>
2013-12-10 08:29:04 +0100

    Fixed import

--- src/test/java/nl/wiggert/cleancode/HelloWorldTest.java
+++ src/test/java/nl/wiggert/cleancode/HelloWorldTest.java
@@ -1,6 +1,6 @@
 package nl.wiggert.cleancode;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
